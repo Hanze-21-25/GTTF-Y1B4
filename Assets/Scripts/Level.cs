c7d554b30b936c25c94392ce648b74acfc4ccdf7
Nosikov Daniil Grigoryevich <danilanosikov@gmail.com>
2022-05-02 17:35:24 +0200

    Added Tile naming automation
    Tiles now are properly named automatically.
    P.S. Edited other code to follow encapsulation principles - security measures.

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Level : MonoBehaviour
 {
     public Transform tilePrefab;
-    private string contentsFolderName = "Contents";
+    private string contentsFolderName = "Tiles";
     private int width;
     private int height;
     private List<Transform> tiles;
@@ -16,6 +16,8 @@ public class Level : MonoBehaviour
 
     void Start()
     {
+        tilePrefab.localScale = new Vector3(tileSize/10, 1, tileSize/10);
+        
         width = (int) size.x;
         height = (int) size.y;
         
@@ -30,41 +32,57 @@ public class Level : MonoBehaviour
     private void GenerateLevel()
     {
         
-        string folderName = contentsFolderName;
-
-        if (transform.Find(folderName))
+        // Destroys previous generation artifacts
+        if (transform.Find(contentsFolderName))
         {
-            DestroyImmediate(transform.Find(folderName).gameObject);
+            DestroyImmediate(transform.Find(contentsFolderName).gameObject);
         }
-
-        Transform folder = new GameObject(folderName).transform;
+        
+        // Creates a folder under an object and puts all the tiles in it
+        var folder = new GameObject(contentsFolderName).transform;
         folder.parent = transform;
         
-        tilePrefab.localScale = new Vector3(tileSize/10, 1, tileSize/10);
+        // Makes sure developer would set proper size for a level.
         if (size.x <= 0 || size.y <= 0)
         {
             throw new Exception("Null Exception - tile amount can not be less than 1");
         }
 
+        //Generation itself
+        tiles = GenerateLevelBody();
+    }
+
+    private List<Transform> GenerateLevelBody()
+    {
+        var tiles = new List<Transform>();
+
         for(var x = 0; x < size.x; x++)
         {
             for(var y = 0; y < size.y; y++)
             {
-                var tile = SpawnTile(x,y,tileOffset);
-                tile.parent = folder;
+                var tile = SpawnTile(x,y,tileOffset, "Tile");
+                tiles.Add(tile);
             }
         }
+
+        return tiles;
     }
-    
-    private Transform SpawnTile(int x, int y, float offset)
+
+    private Transform SpawnTile(int x, int y, float offset, string tileName)
     {
         var position = transform.position;
         
-        var x1 = position.x + tileSize * x * tileOffset + tileSize / 2 - tileSize*size.x/2;
-        var z1 = position.z + tileSize * y * tileOffset + tileSize / 2 - tileSize*size.y/2;
+        var x1 = position.x + tileSize * x * offset + tileSize / 2 - tileSize*size.x/2;
+        var z1 = position.z + tileSize * y * offset + tileSize / 2 - tileSize*size.y/2;
         
         var tilePosition = new Vector3(x1 ,0 ,z1);
-        return Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.zero));
         
+        var tile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.zero));
+        tile.name = tileName + " "+ (x + 1) + "."+ (y + 1);
+        var folder = transform.Find(contentsFolderName);
+        tile.parent = folder;
+        
+        return tile;
+
     }
 }
