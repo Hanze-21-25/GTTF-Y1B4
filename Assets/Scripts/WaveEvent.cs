eefdba1446605ec3744d53ddd3e1bb374d19c132
Nosikov Daniil Grigoryevich <danilanosikov@gmail.com>
2022-05-06 14:02:39 +0200

    mc
    minor change 2

--- Assets/Scripts/WaveEvent.cs
+++ Assets/Scripts/WaveEvent.cs
@@ -5,74 +5,50 @@ using UnityEngine.UI;
 public class WaveSpawner : MonoBehaviour
 {
     public static int EnemiesAlive = 0;
-
     public Wave[] waves;
-
     public Transform spawnPoint;
-
     public float timeBetweenWaves = 5f;
     public float countdown = 10f;
-
     public Text waveCountdownText;
-
     private int waveIndex = 0;
-
+    
     private void Start()
     {
         EnemiesAlive = 0;
     }
 
+    void Update () {
 
-    void Update ()
-    {
-
-        if (EnemiesAlive > 0)
-        {
-            return;
-        }
-
-        if (countdown <= 0f)
-        {
+        if (EnemiesAlive > 0) return;
+        if (countdown <= 0f) {
             StartCoroutine(SpawnWave());
             countdown = timeBetweenWaves;
             return;
         }
-        countdown -= Time.deltaTime;
         
+        countdown -= Time.deltaTime;
         countdown = Mathf.Clamp(countdown, 0f, Mathf.Infinity);
-
         waveCountdownText.text = string.Format("{0:0.0}", countdown);
     }
-
-    IEnumerator SpawnWave ()
-    {
-
+    
+    IEnumerator SpawnWave () {
         Wave wave = waves[waveIndex];
-
-        for (int i = 0; i < wave.count; i++)
-        {
+        for (int i = 0; i < wave.count; i++) {
             SpawnEnemy(wave.enemy);
             yield return new WaitForSeconds(1f / wave.rate);
         }
-
         waveIndex++;
         PlayerStats.Rounds++;
-
-        if (waveIndex == waves.Length)
-        {
+        if (waveIndex == waves.Length) {
             Debug.Log("LEVEL WON!");
             this.enabled = false;
             FindObjectOfType<GameManager>().WinLevel();
         }
-     
     }
-
-    void SpawnEnemy (GameObject enemy)
-    {
-
+    
+    void SpawnEnemy (GameObject enemy) {
         Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
         EnemiesAlive++;
     }
-
-
+    
 }
