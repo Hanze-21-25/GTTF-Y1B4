f941b5c0d5e035bcb17f1e1cf285bab2fd52a0f7
Guillaume Maille <guillaume.maille.le@gmail.com>
2022-05-30 14:54:56 +0200

    Improved Cutscene
    Dialogue is not in order.

--- Assets/Scripts/UI_Assistant.cs
+++ Assets/Scripts/UI_Assistant.cs
@@ -10,35 +10,51 @@ public class UI_Assistant : MonoBehaviour
     private Text messageText;
     private TextWriter.TextWriterSingle textWriterSingle;
     private AudioSource talkingAudioSource;
+    private int counter = 0;
+    public GameObject cutsceneNextUI;
+
+    string[] messageArray = new string[] {
+                        "Okay human, I heard from General X that you should be able to help us fight this war.",
+                        "I heard that you humans use these things called names, what's your's?",
+                        "To be honest, I'm not a big fan of the General's decision to put you in charge.  It's you humans who started this whole thing in the first place!",
+                        "But General X was a great X, he was a great leading figure for all of us animals.",
+                        "I trust he had his reasons in choosing you.",
+                        "Here, let me show you the basics.",
+                        "There are straggling plastic bottles from the previous battle that you can help us get rid of.", };
 
     private void Awake()
+
     {
-        messageText = transform.Find("message").Find("messageText").GetComponent<Text>();
-        talkingAudioSource = transform.Find("talkingSound").GetComponent<AudioSource>();
+        for (int i = 0; i < messageArray.Length; i++)
+        {
 
-        transform.Find("message").GetComponent<Button_UI>().ClickFunc = () => {
-            if (textWriterSingle != null && textWriterSingle.IsActive())
-            {
-                // Currently active TextWriter
-                textWriterSingle.WriteAllAndDestroy();
-            }
-            else
-            {
-                string[] messageArray = new string[] {
-                    "Okay human, I heard from General X that you should be able to help us fight this war.",
-                    "I heard that you humans use these things called names, what's your's?",
-                    "To be honest, I'm not a big fan of the General's decision to put you in charge.  It's you humans who started this whole thing in the first place!",
-                    "But General X was a great X, he was a great leading figure for all of us animals.",
-                    "I trust he had his reasons in choosing you.",
-                    "Here, let me show you the basics.",
-                    "There are straggling plastic bottles from the previous battle that you can help us get rid of.",
-                };
+            messageText = transform.Find("message").Find("messageText").GetComponent<Text>();
+            talkingAudioSource = transform.Find("talkingSound").GetComponent<AudioSource>();
 
-                string message = messageArray[Random.Range(0, messageArray.Length)];
-                StartTalkingSound();
-                textWriterSingle = TextWriter.AddWriter_Static(messageText, message, .02f, true, true, StopTalkingSound);
-            }
+            transform.Find("message").GetComponent<Button_UI>().ClickFunc = () =>
+            {
+                if (textWriterSingle != null && textWriterSingle.IsActive())
+                {
+                    // Currently active TextWriter
+                    textWriterSingle.WriteAllAndDestroy();
+                }
+                if (counter < messageArray.Length)
+                {
+                    string message = messageArray[counter];
+                    Debug.Log(counter + " , " + messageArray.Length);
+                    StartTalkingSound();
+                    textWriterSingle = TextWriter.AddWriter_Static(messageText, message, .02f, true, true, StopTalkingSound);
+                    counter++;
+                }
+                else
+                {
+                    cutsceneNextUI.SetActive(true);
+                }
+            };
+
+              
         };
+        
     }
 
     private void StartTalkingSound()
