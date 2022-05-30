b35f985510aff6faf38da77f93d67f07aad545c8
Guillaume Maille <guillaume.maille.le@gmail.com>
2022-05-30 16:05:04 +0200

    Script Works for All Cutscenes

--- Assets/Scripts/UI_Assistant.cs
+++ Assets/Scripts/UI_Assistant.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using CodeMonkey.Utils;
+using UnityEngine.SceneManagement;
+
 
 public class UI_Assistant : MonoBehaviour
 {
@@ -12,20 +14,45 @@ public class UI_Assistant : MonoBehaviour
     private AudioSource talkingAudioSource;
     private int counter = 0;
     public GameObject cutsceneNextUI;
-
-    string[] messageArray = new string[] {
-                        "Okay human, I heard from General X that you should be able to help us fight this war.",
-                        "I heard that you humans use these things called names, what's your's?",
-                        "To be honest, I'm not a big fan of the General's decision to put you in charge.  It's you humans who started this whole thing in the first place!",
-                        "But General X was a great X, he was a great leading figure for all of us animals.",
-                        "I trust he had his reasons in choosing you.",
-                        "Here, let me show you the basics.",
-                        "There are straggling plastic bottles from the previous battle that you can help us get rid of.", };
+    private int sceneNumber;
 
     private void Awake()
 
     {
-        for (int i = 0; i < messageArray.Length; i++)
+        sceneNumber = SceneManager.GetActiveScene().buildIndex;
+        List<string> messageList = new List<string>() { };
+        switch (sceneNumber)
+        {
+            case 2:
+                List<string> toAdd1 = new List<string>()
+                {
+                    "Okay human, I heard from General X that you should be able to help us fight this war.",
+                    "I heard that you humans use these things called names, what's your's?",
+                    "To be honest, I'm not a big fan of the General's decision to put you in charge.  It's you humans who started this whole thing in the first place!",
+                    "But General X was a great X, he was a great leading figure for all of us animals.",
+                    "I trust he had his reasons in choosing you.",
+                    "Here, let me show you the basics.",
+                    "There are straggling plastic bottles from the previous battle that you can help us get rid of."
+                };
+                messageList.AddRange(toAdd1);
+
+                break;
+
+            case 4:
+                messageList.Clear();
+                List<string> toAdd2 = new List<string>()
+                {
+                    "I saw you take care of those plastic bottles, great job!",
+                    "But the plastic invasion is getting stronger and stronger! Many of our strongest Animals have been captured. Without them, we have no chance of defeating the pollution army!",
+                    "My good friend Slimy Squid was taken by a group of humans to a restaurant not long ago.",
+                    "We’re going to need his help if we want to deal with big groups of plastic!"
+                };
+
+                messageList.AddRange(toAdd2);
+                break;
+        }   
+
+        for (int i = 0; i < messageList.Count; i++)
         {
 
             messageText = transform.Find("message").Find("messageText").GetComponent<Text>();
@@ -38,10 +65,10 @@ public class UI_Assistant : MonoBehaviour
                     // Currently active TextWriter
                     textWriterSingle.WriteAllAndDestroy();
                 }
-                if (counter < messageArray.Length)
+                if (counter < messageList.Count)
                 {
-                    string message = messageArray[counter];
-                    Debug.Log(counter + " , " + messageArray.Length);
+                    string message = messageList[counter];
+                    Debug.Log(counter + " , " + messageList.Count);
                     StartTalkingSound();
                     textWriterSingle = TextWriter.AddWriter_Static(messageText, message, .02f, true, true, StopTalkingSound);
                     counter++;
@@ -52,20 +79,19 @@ public class UI_Assistant : MonoBehaviour
                 }
             };
 
-              
-        };
+            };    
+        }
         
-    }
 
     private void StartTalkingSound()
     {
     talkingAudioSource.Play();
     }
 
-     private void StopTalkingSound()
-     {
-     talkingAudioSource.Stop();
-     }
+    private void StopTalkingSound()
+    {
+    talkingAudioSource.Stop();
+    }
 
     private void Start()
     {
