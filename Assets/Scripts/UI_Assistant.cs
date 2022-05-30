2dad6911c360bb1b7ff568c6b2cb7426b087e979
Filipp Aronovits <filipp.aronovits@gmail.com>
2022-05-30 13:46:57 +0200

    Added Talking to first cutscene

---
+++ Assets/Scripts/UI_Assistant.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
+
+public class UI_Assistant : MonoBehaviour
+{
+
+    private Text messageText;
+    private TextWriter.TextWriterSingle textWriterSingle;
+    private AudioSource talkingAudioSource;
+
+    private void Awake()
+    {
+        messageText = transform.Find("message").Find("messageText").GetComponent<Text>();
+        talkingAudioSource = transform.Find("talkingSound").GetComponent<AudioSource>();
+
+        transform.Find("message").GetComponent<Button_UI>().ClickFunc = () => {
+            if (textWriterSingle != null && textWriterSingle.IsActive())
+            {
+                // Currently active TextWriter
+                textWriterSingle.WriteAllAndDestroy();
+            }
+            else
+            {
+                string[] messageArray = new string[] {
+                    "Okay human, I heard from General X that you should be able to help us fight this war.",
+                    "I heard that you humans use these things called names, what's your's?",
+                    "To be honest, I'm not a big fan of the General's decision to put you in charge.  It's you humans who started this whole thing in the first place!",
+                    "But General X was a great X, he was a great leading figure for all of us animals.",
+                    "I trust he had his reasons in choosing you.",
+                    "Here, let me show you the basics.",
+                    "There are straggling plastic bottles from the previous battle that you can help us get rid of.",
+                };
+
+                string message = messageArray[Random.Range(0, messageArray.Length)];
+                StartTalkingSound();
+                textWriterSingle = TextWriter.AddWriter_Static(messageText, message, .02f, true, true, StopTalkingSound);
+            }
+        };
+    }
+
+    private void StartTalkingSound()
+    {
+    talkingAudioSource.Play();
+    }
+
+     private void StopTalkingSound()
+     {
+     talkingAudioSource.Stop();
+     }
+
+    private void Start()
+    {
+        talkingAudioSource.Stop();
+        //TextWriter.AddWriter_Static(messageText, "This is the assistant speaking, hello and goodbye, see you next time!", .1f, true);
+    }
+
+}
